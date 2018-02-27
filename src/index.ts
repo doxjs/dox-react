efd4d7111f6d7e05bd42c74724a6e1ad6015e2fe
msdimos <msdoubledimos@gmail.com>
2018-02-27 17:06:32 +0800

    first commit

---
+++ src/index.ts
@@ -0,0 +1,69 @@
+import { Component, createElement } from "react";
+import DoxJS from "doxjs";
+import * as PropTypes from "prop-types";
+
+export class Provider<T> extends Component<T, any> {
+    static contextTypes = {
+        dox: PropTypes.object,
+        store: PropTypes.object
+    }
+    static childContextTypes = Provider.contextTypes;
+    private dox: DoxJS<T>;
+
+    constructor(props) {
+        super(props);
+        this.dox = props.dox;
+    }
+
+    getChildContext() {
+        if (this.dox) {
+            return {
+                dox: this.dox,
+                store: this.dox.observe()
+            }
+        } else {
+            let dox = new DoxJS({});
+            return {
+                dox,
+                store: dox.observe()
+            }
+        }
+    }
+
+    render() {
+        return this.props.children;
+    }
+
+}
+
+export function connect(component) {
+
+    return class HOC<T> extends Component<any, any> {
+        static contextTypes = {
+            dox: PropTypes.object,
+            store: PropTypes.object
+        }
+        private dox: DoxJS<T>;
+
+        constructor(props) {
+            super(props);
+            this.dox = this.context.dox;
+        }
+
+        render() {
+            return createElement(component, {
+                ...this.props,
+                dox: this.dox,
+                store: this.dox.observe()
+            }, this.props.children);
+        }
+    }
+
+}
+
+class A extends Component {
+
+}
+
+
+console.log([connect(A)]);
